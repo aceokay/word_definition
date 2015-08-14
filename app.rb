7ce3e9865c9687cdf512e0f2ce4455d6abe8bd90
Austin <aokincaid@gmail.com>
2015-08-14 11:31:58 -0700

    Add front page ability to display call-to-action if no words have yet been provided

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
-require('./lib/description')
+require('./lib/definition')
 require('pry')
 also_reload('/lib/**/*.rb')
 
 get('/') do
+  words = Word.all()
+  @word_list_strings = []
+  words.each() do |word|
+    @word_list_strings.push(word.word())
+  end
+  if words.length() < 1
+    help_message = "There aren't any words yet! Help us out and put some in!"
+    @word_list_strings.push(help_message)
+  end
   erb(:index)
 end
